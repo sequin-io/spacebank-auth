0b55e09cd4289ee756937cf225406717db2cacac
Kayky de Brito <oss@kaykyb.com>
2020-09-22 14:00:22 -0300

    Disable auto reset on transactions table

--- src/components/TransactionsTable.tsx
+++ src/components/TransactionsTable.tsx
@@ -80,6 +80,7 @@ export default function TransactionsTable({
   return (
     <Table
       data={filteredData}
+      autoReset={false}
       onSelectRow={onSelectRow}
       columns={memoizedColumns as any}
     />
